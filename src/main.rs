44574bb5a280109cd02f22b1cfd574e9d9e645b6
Xanthus <Xanthus58@protonmail.com>
2022-06-10 06:23:32 +0930

    1.0.2
    > Added a delay so the message is actually visible

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::{thread, time::Duration};
+
 fn cls(){
     print!("{esc}c", esc = 27 as char); 
 }
 fn main() {
     cls();
     println!("space is cool :O");
+    thread::sleep(Duration::from_secs(5));
 }
